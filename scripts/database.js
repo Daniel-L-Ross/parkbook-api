4eb4b1e3965b448dcea18f444121426246d8614e
Dan Ross <daniellross@gmail.com>
2021-03-15 14:33:11 -0500

    copied data from database, implemented code to add id to data

---
+++ scripts/database.js
@@ -0,0 +1,44 @@
+let allparks = []
+
+export const useParks = () => {
+    return allparks.slice()
+}
+
+export const getParks = () => {
+    return fetch("http://localhost:8088/parks")
+        .then(res => res.json())
+        .then(
+            response => {
+                allparks = response
+            }
+                )
+}
+export const getNewParks = () => {
+    return fetch("http://localhost:8088/newParks")
+        .then(res => res.json())
+        .then(
+            response => {
+                console.table(response)
+            }
+                )
+}
+
+
+export const addPark = parkObj => {
+    return fetch("http://localhost:8088/newParks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(parkObj)
+    })
+}
+
+// let id = 1
+// let newParks = []
+// parksData.forEach(park => {
+//     park.id = id
+//     newParks.push(park)
+//     id += 1
+// })
+
